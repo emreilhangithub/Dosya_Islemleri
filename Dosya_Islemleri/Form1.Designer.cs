c7cd888ff6cf539f170d74299e093fad92f03ea5
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-06-01 12:56:36 +0300

    metin belgesi oluşturmayı ögrendik bu kısımda dosya yolunu seçiyoruz ve dosya adını yazdıktan sonra oluştura basıyoruz

--- Dosya_Islemleri/Form1.Designer.cs
+++ Dosya_Islemleri/Form1.Designer.cs
@@ -35,6 +35,12 @@ namespace Dosya_Islemleri
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -71,12 +77,70 @@ namespace Dosya_Islemleri
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(731, 211);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(119, 37);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Oluştur";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(731, 112);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 37);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Yol Seç";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(476, 211);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(221, 37);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(476, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 37);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(340, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 29);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Dosya Adı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 29);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Dosya Yolu:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LimeGreen;
             this.ClientSize = new System.Drawing.Size(995, 578);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -85,6 +149,7 @@ namespace Dosya_Islemleri
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -96,6 +161,12 @@ namespace Dosya_Islemleri
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Dosya_Islemleri/Form1.cs
+++ Dosya_Islemleri/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;  //kütüptane ekledik StreamWriter için
 
 namespace Dosya_Islemleri
 {
@@ -16,6 +17,10 @@ namespace Dosya_Islemleri
         {
             InitializeComponent();
         }
+
+        string dosyaadi, dosyayolu;
+        StreamWriter sw; //akış yazıcı
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -32,15 +37,22 @@ namespace Dosya_Islemleri
         {
             folderBrowserDialog1.ShowDialog();
         }
+       
 
-        private void button5_Click(object sender, EventArgs e)
+        private void button5_Click_1(object sender, EventArgs e)
         {
-
+            dosyaadi = textBox2.Text;
+            sw = File.CreateText(dosyayolu+"\\"+dosyaadi+".txt"); //metin belgesi oluştur methodunu 
+            sw.Close(); //yazma işlemini durdur
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void button4_Click_1(object sender, EventArgs e)
         {
-
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK) //ok tusuna basarsam
+            {
+                dosyayolu = folderBrowserDialog1.SelectedPath.ToString(); //seçilen yolu string formatta tut
+                textBox1.Text = dosyayolu; //ve dosya yolunu buraya yaz
+            }
         }
     }
 }
